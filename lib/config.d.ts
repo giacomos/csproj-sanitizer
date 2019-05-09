5b70a0dceedf10f4098db6f8e980def59651efa9
Giacomo Spettoli <giacomo.spettoli@gmail.com>
2019-05-09 15:44:35 +0200

    remove built code from repo.

--- lib/config.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const DEFAULT_FIND_PATTERN = "**/*.{cshtml,cs}";
-export declare const DEFAULT_FIND_IGNORE = "{node_modules,obj,bin}/**";
--- lib/csprojSanitizer.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const findStringLines: (data: string, searchKeyword: string) => number[];
-declare const csprojSanitizer: ({ filePath, findPattern, findIgnores, rootDir }: Params) => Promise<Result>;
-export default csprojSanitizer;
